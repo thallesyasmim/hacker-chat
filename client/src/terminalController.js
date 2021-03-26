fe9bb5878331c014bea2b33859fb15ca1505afdc
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-26 10:10:01 -0300

    on input received

--- client/src/terminalController.js
+++ client/src/terminalController.js
@@ -1,7 +1,24 @@
+import ComponentsBuilder from "./components.js";
+
 export default class TerminalController {
-    constructor() {}
+    constructor() { }
+
+    #onInputReceived(eventEmitter) {
+        return function () {
+            const message = this.getValue()
+            console.log(message)
+            this.clearValue()
+        }
+    }
 
     async initializeTable(eventEmitter) {
+        const components = new ComponentsBuilder()
+            .setScreen({ title: 'HackerChat - Thalles Gabriel & Yasmim Cristina' })
+            .setLayoutComponent()
+            .setInputComponent()
+            .build()
 
+        components.input.focus()
+        components.screen.render()
     }
 }
