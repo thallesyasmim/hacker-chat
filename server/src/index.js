6399470e7699fd51e661821ab1f0e51dc484f2bf
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-27 20:36:25 -0300

    connection client and server

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,7 @@
 import SocketServer from "./socket.js";
 import Event from 'events'
+import { constants } from "./constants.js";
+
 
 const eventEmitter = new Event()
 
@@ -7,3 +9,39 @@ const port = process.env.PORT || 9898
 const socketServer = new SocketServer({ port })
 const server = await socketServer.initialize(eventEmitter)
 console.log('socket server is running at', server.address().port)
+
+/*
+eventEmitter.on(constants.event.NEW_USER_CONNECTED, (socket) => {
+    console.log('new connection!!', socket.id)
+    socket.on('data', data => {
+        console.log('server received', data.toString())
+        socket.write('world!')
+    })
+})
+
+async function testServer() {
+    const options = {
+        port: 9898,
+        host: 'localhost',
+        headers: {
+            Connection: 'Upgrade',
+            Upgrade: 'websocket'
+        }
+    }
+
+    const http = await import('http')
+    const req = http.request(options)
+    req.end() // dispara
+
+    req.on('upgrade', (res, socket) => {
+        socket.on('data', data => {
+            console.log('client received', data.toString())
+        })
+
+        setInterval(() => {
+            socket.write('Hello!')
+        }, 500)
+    })
+}
+await testServer()
+*/
