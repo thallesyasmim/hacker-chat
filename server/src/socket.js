7ab93c0cb887da9a0061dda818e26cae41a2698a
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-27 20:36:43 -0300

    emit event

--- server/src/socket.js
+++ server/src/socket.js
@@ -1,3 +1,4 @@
+import { constants } from './constants.js'
 import http from 'http'
 import { v4 } from 'uuid'
 
@@ -22,6 +23,7 @@ export default class SocketServer {
             ].map(line => line.concat('\r\n')).join('')
 
             socket.write(headers)
+            eventEmitter.emit(constants.event.NEW_USER_CONNECTED, socket)
         })
 
         return new Promise((resolve, reject) => {
