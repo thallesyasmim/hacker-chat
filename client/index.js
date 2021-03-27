e9a4fc84a1285e611a275fa0e09d683e4ae1a012
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-27 18:37:19 -0300

    process.argv

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
+/*
+node index.js \
+    --username thalles \
+    --room sala01 \
+    --hostUri localhost
+*/ 
+
 import Events from 'events'
 import TerminalController from "./src/terminalController.js";
+
+const [nodePath, filePath, ...commands ] = process.argv
+
+console.log(commands)
 
 const componentEmitter = new Events()
 
