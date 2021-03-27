88517ac82323fd29414bd74b0612d7eca5fcb06f
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-27 19:42:45 -0300

    initialize socket server

---
+++ server/src/index.js
@@ -0,0 +1,8 @@
+import SocketServer from "./socket.js";
+import Event from 'events'
+
+const eventEmitter = new Event()
+
+const port = process.env.PORT || 9898
+const socketServer = new SocketServer({ port })
+const server = await socketServer.initialize(eventEmitter)
