50c0bc62f08ba601b0f7c2a63f626c7f62a827cf
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-27 19:20:36 -0300

    commands

--- client/index.js
+++ client/index.js
@@ -6,13 +6,14 @@ node index.js \
 */ 
 
 import Events from 'events'
+import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
 
 const [nodePath, filePath, ...commands ] = process.argv
-
-console.log(commands)
+const config = CliConfig.parseArguments(commands)
+console.log('config', config)
 
 const componentEmitter = new Events()
 
-const controller = new TerminalController()
-await controller.initializeTable(componentEmitter)
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
