20a5e9d492fa9c67a655f207e5a844354cc65f0f
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-27 14:43:48 -0300

    get user color

--- client/src/terminalController.js
+++ client/src/terminalController.js
@@ -1,11 +1,24 @@
 import ComponentsBuilder from "./components.js";
 
 export default class TerminalController {
+    #usersColors = new Map() 
+
     constructor() { }
 
-    pickColor() {
+    #pickColor() {
         return `#${((1 << 24) * Math.random() | 0).toString(16)}-fg`
     }
+
+    #getUserColor(userName) {
+        if(this.#usersColors.has(userName)) 
+            return this.#usersColors.get(userName)
+
+        const color = this.#pickColor()
+        this.#usersColors.set(userName, color)
+
+        return color
+    }
+
 
     #onInputReceived(eventEmitter) {
         return function () {
