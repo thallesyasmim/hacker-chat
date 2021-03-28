493592736cc39f20a9e9dbe622437685699446be
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-27 23:04:41 -0300

    initialize socket client

--- client/index.js
+++ client/index.js
@@ -8,10 +8,12 @@ node index.js \
 import Events from 'events'
 import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
+import SocketClient from './src/socket.js'
 
 const [nodePath, filePath, ...commands ] = process.argv
 const config = CliConfig.parseArguments(commands)
-console.log('config', config)
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
 
 const componentEmitter = new Events()
 
