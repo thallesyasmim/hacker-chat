dcaadbb01f6ba43564eaa60f787a6b3fe96d847b
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-28 20:16:59 -0300

    join room with attach events

--- client/index.js
+++ client/index.js
@@ -9,13 +9,24 @@ import Events from 'events'
 import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
 import SocketClient from './src/socket.js'
+import EventManager from './src/eventManager.js';
 
 const [nodePath, filePath, ...commands ] = process.argv
 const config = CliConfig.parseArguments(commands)
+
+const componentEmitter = new Events()
 const socketClient = new SocketClient(config)
 await socketClient.initialize()
 
-const componentEmitter = new Events()
+const eventManager = new EventManager({ componentEmitter, socketClient })
+const events = eventManager.getEvents()
+socketClient.attachEvents(events)
+const data = {
+    roomId: config.room,
+    userName: config.username
+}
 
-// const controller = new TerminalController()
-// await controller.initializeTable(componentEmitter)
+eventManager.joinRoomAndWaitForMessages(data)
+
+const controller = new TerminalController()
+await controller.initializeTable(componentEmitter)
