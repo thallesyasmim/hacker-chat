21e7655c81583117f58321274ab9b4b040626130
Thalles Gabriel <thallesgabriel1307@gmail.com>
2021-03-28 23:28:40 -0300

    comments

--- client/index.js
+++ client/index.js
@@ -1,9 +1,23 @@
+#!/usr/bin/env node
+
 /*
+npm i -g @thallesyasmim/hacker-chat-client
+npm unlink -g @thallesyasmim/hacker-chat-client
+
+    chmod +x index.js
+    npm link
+    npm login
+    npm publish --access public
+
+hacker-chat \
+    --username erickwendel \
+    --room sala01
+
 node index.js \
     --username thalles \
     --room sala01 \
     --hostUri localhost
-*/ 
+*/
 
 import Events from 'events'
 import CliConfig from './src/cliConfig.js';
@@ -11,7 +25,7 @@ import TerminalController from "./src/terminalController.js";
 import SocketClient from './src/socket.js'
 import EventManager from './src/eventManager.js';
 
-const [nodePath, filePath, ...commands ] = process.argv
+const [nodePath, filePath, ...commands] = process.argv
 const config = CliConfig.parseArguments(commands)
 
 const componentEmitter = new Events()
